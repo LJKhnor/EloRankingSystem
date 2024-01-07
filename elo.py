4bf9685fec11b996148be3c0cbc2f666fc6ee1b1
yukimushide92 <gillesrosier92@outlook.com>
2024-01-07 13:46:01 +0100

    fix doublons

--- elo.py
+++ elo.py
@@ -31,13 +31,6 @@ class Implementation:
         """
         return self.decks
 
-    
-    def __getDeckList(self):
-        """
-        Returns this implementation's decks list.
-        @return - the list of all deck objects in the implementation.
-        """
-        return self.decks
 
     def getPlayer(self, name):
         """
@@ -61,30 +54,11 @@ class Implementation:
                 return deck
         return None
 
-    
-    def getDeck(self, name):
-        """
-        Returns the deck in the implementation with the given name.
-        @param name - name of the deck to return.
-        @return - the deck with the given name.
-        """
-        for deck in self.decks:
-            if deck.name == name:
-                return deck
-        return None
-
     def getPlayers(self, players):
         """
         Return all the players in the list
         """
         return " ,".join(players)
-    
-    def getDecks(self, decks):
-        """
-        Return all the decks in the list
-        """
-        return " ,".join(decks)
-
     
     def getDecks(self, decks):
         """
@@ -125,17 +99,6 @@ class Implementation:
 
         self.decks.append(_Deck(name=name, rating=rating))
 
-    def addDeck(self, name, rating=None):
-        """
-        Adds a new deck to the implementation.
-        @param name - The name to identify a specific deck.
-        @param rating - The decks's rating.
-        """
-        if rating == None:
-            rating = self.base_rating
-
-        self.decks.append(_Deck(name=name, rating=rating))
-
     def addPlayers(self, players, rating=None):
         """
         Add a list of players to the implementation class
@@ -154,14 +117,6 @@ class Implementation:
         for deck in decks:
             self.addDeck(deck, rating)
 
-    def addDecks(self, decks, rating=None):
-        """
-        Add a list of decks to the implementation class
-        @param decks - the decks list
-        @param rating - the global rating for all decks
-        """
-        for deck in decks:
-            self.addDeck(deck, rating)
 
     def removePlayer(self, name):
         """
@@ -176,16 +131,8 @@ class Implementation:
         Remove a deck to the implementation.
         @param name - The name to identify a specific deck.
         """
-        self.__getDeckList().remove(self.getDeck(name))
+        self.__getDeckList().remove(self.getDeck(name))   
 
-    def recordMatch(self, name1, deckName1, name2, deckName2, winner=None, draw=False):
-    
-    def removeDeck(self, name):
-        """
-        Remove a deck to the implementation.
-        @param name - The name to identify a specific deck.
-        """
-        self.__getDeckList().remove(self.getDeck(name))
 
     def recordMatch(self, name1, deckName1, name2, deckName2, winner=None, draw=False):
         """
@@ -195,7 +142,7 @@ class Implementation:
         """
         players = []
         decks = []
-        decks = []
+
         for player in self.players:
             players.append(player.name)
         
@@ -203,15 +150,9 @@ class Implementation:
             decks.append(deck.name)
 
         if (name1 in players and name2 in players and deckName1 in decks and deckName2 in decks):
-        
-        for deck in self.decks:
-            decks.append(deck.name)
 
-        if (name1 in players and name2 in players and deckName1 in decks and deckName2 in decks):
             player1 = self.getPlayer(name1)
             player2 = self.getPlayer(name2)
-            deck1 = self.getDeck(deckName1)
-            deck2 = self.getDeck(deckName2)
             deck1 = self.getDeck(deckName1)
             deck2 = self.getDeck(deckName2)
 
@@ -219,17 +160,13 @@ class Implementation:
             expected2 = player2.compareRating(player1)
             deckExpected1 = deck1.compareRating(deck2)
             deckExpected2 = deck2.compareRating(deck1)
-            deckExpected1 = deck1.compareRating(deck2)
-            deckExpected2 = deck2.compareRating(deck1)
+
 
             k = len(self.__getPlayerList()) * 42
-            deck_k = len(self.__getDeckList()) * 42
             deck_k = len(self.__getDeckList()) * 42
 
             rating1 = player1.rating
             rating2 = player2.rating
-            deckRating1 = deck1.rating
-            deckRating2 = deck2.rating
             deckRating1 = deck1.rating
             deckRating2 = deck2.rating
 
@@ -238,13 +175,9 @@ class Implementation:
                 score2 = 0.5
                 deckScore1 = 0.5
                 deckScore2 = 0.5
-                deckScore1 = 0.5
-                deckScore2 = 0.5
             elif winner == name1:
                 score1 = 1.0
                 score2 = 0.0
-                deckScore1 = 1.0
-                deckScore2 = 0.0
                 deckScore1 = 1.0
                 deckScore2 = 0.0
             elif winner == name2:
@@ -252,15 +185,11 @@ class Implementation:
                 score2 = 1.0
                 deckScore1 = 0.0
                 deckScore2 = 1.0
-                deckScore1 = 0.0
-                deckScore2 = 1.0
             else:
                 raise Exception('One of the names must be the winner or draw must be True')
 
             newRating1 = rating1 + k * (score1 - expected1)
             newRating2 = rating2 + k * (score2 - expected2)
-            newDeckRating1 = deckRating1 + deck_k * (deckScore1 - deckExpected1)
-            newDeckRating2 = deckRating2 + deck_k * (deckScore2 - deckExpected2)
             newDeckRating1 = deckRating1 + deck_k * (deckScore1 - deckExpected1)
             newDeckRating2 = deckRating2 + deck_k * (deckScore2 - deckExpected2)
 
@@ -280,13 +209,6 @@ class Implementation:
                 newDeckRating2 = 0
                 newDeckRating1 = newDeckRating1 - newDeckRating2
 
-            if newDeckRating1 < 0:
-                newDeckRating1 = 0
-                newDeckRating2 = deckRating2 - deckRating1
-
-            elif newDeckRating2 < 0:
-                newDeckRating2 = 0
-                newDeckRating1 = newDeckRating1 - newDeckRating2
 
             player1.rating = newRating1
             player2.rating = newRating2
@@ -294,8 +216,6 @@ class Implementation:
             deck1.rating = newDeckRating1
             deck2.rating = newDeckRating2
 
-            deck1.rating = newDeckRating1
-            deck2.rating = newDeckRating2
         else:
             raise Exception("One or more player you provide don't exist in this Elo league.")
 
@@ -316,18 +236,7 @@ class Implementation:
         """
         deck = self.getDeck(name)
         return deck.rating
-
-    def getPlayersRatingList(self):
     
-    def getDeckRating(self, name):
-        """
-        Returns the rating of the player with the given name.
-        @param name - name of the player.
-        @return - the rating of the player with the given name.
-        """
-        deck = self.getDeck(name)
-        return deck.rating
-
     def getPlayersRatingList(self):
         """
         Returns a list of tuples in the form of ({name},{rating})
@@ -337,17 +246,6 @@ class Implementation:
         for player in self.__getPlayerList():
             lst.append((player.name, round(player.rating, 2)))
         return sorted(lst, key=lambda player: player[1], reverse=True)
-    
-    def getDecksRatingList(self):
-        """
-        Returns a list of tuples in the form of ({name},{rating})
-        @return - the list of tuples
-        """
-        lst = []
-        for deck in self.__getDeckList():
-            lst.append((deck.name, round(deck.rating, 2)))
-        return sorted(lst, key=lambda deck: deck[1], reverse=True)
-
     
     def getDecksRatingList(self):
         """
@@ -409,27 +307,4 @@ class _Deck:
 
     def __str__(self):
         return "{} ({})".format(self.name, round(self.rating, 2))
-class _Deck:
-    """
-    A class to represent a player in the Elo Rating System
-    """
-
-    def __init__(self, name, rating):
-        """
-        Runs at initialization of class object.
-        @param name - player name
-        @param rating - player rating
-        """
-        self.name = name
-        self.rating = rating
-
-    def compareRating(self, opponent):
-        """
-        Compares the two ratings of the this player and the opponent.
-        @param opponent - the player to compare against.
-        @returns - The expected score between the two players.
-        """
-        return (1 + 10 ** ((opponent.rating - self.rating) / 400.0)) ** -1
-
-    def __str__(self):
-        return "{} ({})".format(self.name, round(self.rating, 2))
+    
