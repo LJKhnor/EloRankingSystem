bba5cde86b43d189315676b7001a0ee6c09f7c6a
yukimushide92 <gillesrosier92@outlook.com>
2024-01-14 01:02:53 +0100

    add unit test for deck and players

---
+++ projet/tests/test_deck.py
@@ -0,0 +1,27 @@
+import unittest
+from projet.model.deck import _Deck
+class TestDeck(unittest.TestCase):
+
+    def setUp(self):
+        self.deck1 = _Deck("deck 1", 1500.00)
+        self.deck2 = _Deck("deck 2", 1500.00)
+
+    def test_compareRating(self):
+        self.assertEqual(self.deck1.compareRating(self.deck2), 0.5)
+        self.assertEqual(self.deck2.compareRating(self.deck1), 0.5)
+
+        self.deck2.rating = 1600
+        self.assertEqual(self.deck1.compareRating(self.deck2), 0.35993500019711494)
+
+        self.deck2.rating = 1400
+        self.assertEqual(self.deck1.compareRating(self.deck2), 0.6400649998028851)
+
+        self.deck2.rating = 1250
+        self.assertEqual(self.deck1.compareRating(self.deck2), 0.8083176725494586)
+
+    def test_str(self):
+        self.assertEqual(str(self.deck1), "deck 1 (1500.0)")
+        self.assertEqual(str(self.deck2), "deck 2 (1500.0)")
+
+if __name__ == '__main__':
+    unittest.main()
