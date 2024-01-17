aecd3a145b857035c4b188230c2f829024097315
Joachim <etu44137@henallux.be>
2024-01-17 23:02:01 +0100

    début de la création des tables
    #JOL_db_persistance

--- projet/schema.sql
+++ projet/schema.sql
@@ -1,8 +1,43 @@
-DROP TABLE IF EXISTS user;
+drop table if exists user;
+drop table if exists player;
+drop table if exists deck;
+drop table if exists league;
+drop table if exists team;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+drop table if exists match;
+
+create table user (
+    id integer primary key autoincrement,
+    username text unique not null,
+    email text unique not null,
+    password text not null
+);
+
+create table player (
+    id integer primary key autoincrement,
+    name text
+);
+
+create table deck (
+    id integer primary key autoincrement,
+    name text
+);
+
+create table league (
+    id integer primary key autoincrement,
+    label text,
+    type text,
+    start_date timestamp,
+    end_ate timestamp
+);
+
+create table team (
+    id integer primary key autoincrement,
+    name text
+);
+
+create table match (
+    id primary key autoincrement,
+    league_id integer,
+    date timestamp
 );
